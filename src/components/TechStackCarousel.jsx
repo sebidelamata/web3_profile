623596c01d9699a66d931470611d3a19130e71fa
Miguel  de la Mata <sebidelamata@gmail.com>
2024-03-11 09:19:19 -0600

    solved animation issue

--- src/components/TechStackCarousel.jsx
+++ src/components/TechStackCarousel.jsx
@@ -1,4 +1,55 @@
+import { useEffect } from "react"
+
 const TechStackCarousel = () => {
+
+    useEffect(() => {
+        // infinite scroller for tech stack icons
+        const scrollers = document.querySelectorAll('.scroller');
+        // only apply to users who dont have reduce motion turned on
+        const addAnimation = () => {
+            scrollers.forEach((scroller) => {
+                scroller.setAttribute('data-animated', true);
+
+                const scrollerList = scroller.querySelector(".tech-stack-carousel-list");
+                const scrollerContent = Array.from(scrollerList.children);
+
+                scrollerContent.forEach((item) => {
+                    const duplicatedItem = item.cloneNode(true);
+                    duplicatedItem.setAttribute('aria-hidden', true);
+                    scrollerList.appendChild(duplicatedItem);
+                })
+            });
+        }
+
+        if(!window.matchMedia('(prefers-reduced-motion: reduce)').matches){
+            addAnimation();
+        }
+
+        // need to do js scrolling to override github pages bullshit
+        document.addEventListener('DOMContentLoaded', function () {
+            // Get all elements with the class "scroll-link"
+            var scrollLinks = document.querySelectorAll('.scroll-link');
+            
+            // Add click event listener to each scroll link
+            scrollLinks.forEach(function (scrollLink) {
+                scrollLink.addEventListener('click', function (event) {
+                // Prevent default behavior of the anchor link
+                event.preventDefault();
+            
+                // Get the target element's ID from the href attribute
+                var targetId = scrollLink.getAttribute('href').substring(1);
+            
+                // Find the target element by its ID
+                var targetElement = document.getElementById(targetId);
+            
+                // Scroll to the target element using smooth behavior
+                if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                }
+                });
+            });
+            });
+    },[])
     
     return(
         <div className="tech-stack-carousel-scroller scroller">
