bcab235b6a9dee1ab1ca38955698aa56ff4b9dc4
Miguel  de la Mata <sebidelamata@gmail.com>
2024-03-13 22:59:31 -0600

    minted nft on testnet

--- src/routes/Mint.jsx
+++ src/routes/Mint.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import { useProvider, useAccount } from "../EthersProvider";
 import Navbar from "../components/Navbar"
-import { min } from "bn.js";
+import { ethers } from "ethers";
+import portfolioNFTArtifact from '../../artifacts/contracts/PortfolioNFT.sol/MERNS.json';
 
 const Mint = () => {
     
-    const [minted, setMinted] = useState(false);
+    const [minted, setMinted] = useState(null);
     const [loading, setLoading] = useState(false);
 
     const provider = useProvider();
@@ -15,13 +16,18 @@ const Mint = () => {
         try{
             setLoading(true)
             const signer = await provider.getSigner()
-            console.log(signer)
-
-
+            console.log(signer.address)
+            const contractAddress = '0x61989B222F0B93796E5c5f21CC44d95CAADe1B16'
+            const contractABI = portfolioNFTArtifact.abi
+            console.log(contractABI)
+            const contract = new ethers.Contract(contractAddress, contractABI, signer);
+            const tx = await contract.safeMint(signer.address);
+            await tx.wait()
+            setMinted(tx)
         } catch(err) {
-            console.log(err)
+            console.error(err);
         } finally {
-            console.log('tada')
+            setLoading(false);
         }
     }
 
@@ -29,7 +35,10 @@ const Mint = () => {
         <>
             <Navbar></Navbar>
             <div>Mint not implemented</div>
-            <button onClick={mintNFT}>Mint</button>
+            <button onClick={mintNFT}>
+                {loading ? "Minting..." : "Mint NFT"}
+            </button>
+            {console.log(minted)}
         </>
     )
 }
