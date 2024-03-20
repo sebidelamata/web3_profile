a568839f7eeb062a9dc0a7f8767a526b7bd11667
Miguel  de la Mata <sebidelamata@gmail.com>
2024-03-20 16:29:04 -0600

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -28,7 +28,7 @@ const arbitrum = {
 const metadata = {
   name: 'Sebi de la Mata | Full-Stack Web3 Development',
   description: 'A Web3 Portfolio',
-  url: 'https://mywebsite.com', // origin must match your domain & subdomain
+  url: 'https://www.sebidelamata.com', // origin must match your domain & subdomain
   icons: ['https://avatars.mywebsite.com/']
 }
 
