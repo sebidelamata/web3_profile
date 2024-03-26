36791cf772be98e91f70059cd295382bbdf0824c
Miguel  de la Mata <sebidelamata@gmail.com>
2024-03-26 15:49:40 -0600

    router converted to ts

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './routes/App.jsx'
-import Mint from './routes/Mint.jsx'
-import Resume from './routes/Resume.jsx'
-import Blog from './routes/Blog.jsx'
-import { EthersProvider } from './EthersProvider.tsx'
+import React from 'react'
+import App from './routes/App'
+import Mint from './routes/Mint'
+import Resume from './routes/Resume'
+import Blog from './routes/Blog'
+import { EthersProvider } from './EthersProvider'
 
-const Router = () => {
+const Router: React.FC = () => {
     const router = createBrowserRouter([
         {
           path: "/",
@@ -16,11 +17,11 @@ const Router = () => {
           element: <Blog />,
         },
         {
-          path: "mint",
+          path: "/mint",
           element: <Mint />,
         },
         {
-          path: "resume",
+          path: "/resume",
           element: <Resume />,
         },
     ]);
