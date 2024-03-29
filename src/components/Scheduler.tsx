5e0537dc1d7d64f7af2e515c328598d71a8c1a16
Miguel  de la Mata <sebidelamata@gmail.com>
2024-03-29 17:29:16 -0600

    added ts icon to tech stack carousel

--- src/components/Scheduler.tsx
+++ src/components/Scheduler.tsx
@@ -9,7 +9,7 @@ const Scheduler: React.FC = () => {
       <PopupButton
       url="https://calendly.com/sebidelamata/discoveryourweb3vision"
       className="scheduler-button"
-      text="Click to Meet"
+      text="Schedule Meetup"
       /*
         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
