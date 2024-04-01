aa692f2e86343bb9a7a14323a263672e7e73527b
Miguel  de la Mata <sebidelamata@gmail.com>
2024-04-01 15:48:18 -0600

    hardhat config file update

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -9,11 +9,16 @@ module.exports = {
     arbitrumSepolia: {
       url: process.env.VITE_INFURA_TESTNET_RPC,
       accounts: [process.env.VITE_TESTNET_DEV_ACCOUNT0_KEY],
+    },
+    arbitrum: {
+      url: process.env.VITE_INFURA_ARBITRUM_RPC,
+      accounts: [process.env.VITE_TESTNET_DEV_ACCOUNT0_KEY],
     }
   },
   etherscan: {
     apiKey: {
       arbitrumSepolia: process.env.VITE_ARBISCAN_API_KEY,
+      arbitrum: process.env.VITE_ARBISCAN_API_KEY,
     },
     customChains: [
       {
@@ -22,6 +27,14 @@ module.exports = {
         urls: {
             apiURL: "https://api-sepolia.arbiscan.io/api",
             browserURL: "https://sepolia.arbiscan.io/",
+        },
+      },
+      {
+        network: "arbitrum",
+        chainId: process.env.VITE_ARBITRUM_CHAINID,
+        urls: {
+            apiURL: "https://api.arbiscan.io/api",
+            browserURL: "https://arbiscan.io/",
         },
       },
     ],
---
+++ scripts/arbitrumDeploy.cjs
@@ -0,0 +1,39 @@
+const hre = require("hardhat");
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+require('dotenv').config();
+
+async function main() {
+
+    // deploy to blockchain
+    const [deployer] = await hre.ethers.getSigners();
+    const NFT = await hre.ethers.getContractFactory("BiP");
+    const nft = await NFT.deploy(deployer);
+    await nft.waitForDeployment();
+    const contractAddress = await nft.getAddress()
+    console.log("Boxers in Predicaments contract deployed to:", contractAddress);
+    await nft.setBaseTokenUri(process.env.VITE_IPFS_METADATA_BASE_URI)
+    console.log(await nft.getBaseURI())
+
+    const envFilePath = '.env';
+    const parsedEnv = dotenv.parse(fs.readFileSync(envFilePath));
+    const envVariableName = 'VITE_TESTNET_CONTRACT_ADDRESS';
+    if (envVariableName in parsedEnv) {
+        delete parsedEnv[envVariableName];
+    }
+    parsedEnv[envVariableName] = contractAddress;
+    const updatedEnvContent = Object.entries(parsedEnv)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('\n');
+    fs.writeFileSync(envFilePath, updatedEnvContent);
+
+    console.log('Contract address written to contractConfig.js and .env file successfully.');
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
